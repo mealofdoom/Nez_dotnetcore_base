0858377cf07af0151f74236f3dda58922e70b19d
mealofdoom <phallicassortment@gmx.com>
2020-03-02 00:55:25 -0300

    Initial commit with basic console application

---
+++ NezBasicWindow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NezBasicWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
